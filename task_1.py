ca578aa99aaf499aec7232a8c21b2d18e6bd2e85
Alice <moalisa537@gmail.com>
2025-12-20 18:34:28 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -1,10 +1,16 @@
-numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
 
-# TODO заменить значение пропущенного элемента средним арифметическим
-a = numbers[:4] #Числа до None
-b = numbers[5:] #Числа после None
-count_of_numbers = len(numbers)
-srednee_numbers = sum(a+b)/count_of_numbers
+# TODO Посчитайте количество месяцев, которое можно протянуть без долгов
+count_months = 0
+delta = spend - salary
 
-numbers [4] = srednee_numbers
-print("Измененный список:", numbers)
+while money_capital >= delta:
+    money_capital -= delta
+    count_months += 1
+    spend *= (1 + increase)
+    delta = spend - salary
+
+print("Количество месяцев, которое можно протянуть без долгов:", count_months)
